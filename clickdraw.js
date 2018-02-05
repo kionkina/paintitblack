be2cf7dca88bf7938a9aefe1e051f3ae6d1b6ed0
Karina Ionkina <kionkina@stuy.edu>
2018-02-05 13:15:21 -0500

    a crime

--- clickdraw.js
+++ clickdraw.js
@@ -4,6 +4,8 @@
 //2018-02-03  
 
 
+var x1;
+var y1;
 
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
@@ -21,18 +23,23 @@ c.addEventListener("click", function(e) { draw(e); });
 var draw = function(e){
     if (typeof draw_dot != 'number'){
 	{
-	draw_dot = 1;
-	draw_box = 0;
-	return drawCircle(e);
+	    draw_dot = 1;
+	    draw_box = 0;
+
+	    return drawCircle(e);
 	}
     }
     else {
 	if (draw_dot === 1){
 	    console.log("drawing cirlce . . .")
-	    return drawCircle(e);
+
+	    drawCircle(e);
+
 	}
 	else {
-	    return drawRect(e);
+
+	    drawRect(e);
+	    return ctx.beginPath();
 	}
     }
 }
@@ -46,62 +53,45 @@ var clrCanvas = function(e){
 var togl = function(e){
     console.log(" draw_dot was...");
     console.log(draw_dot);
-       if (draw_dot === 1){
+    if (draw_dot === 1){
     	draw_dot = 0;
     	draw_box = 1;
-       }
-        else {
+    }
+    else {
     	draw_box = 0;
     	draw_dot = 1;
-        }
-        console.log(" draw_dot is now...");
-        console.log(draw_dot);
     }
-
-var drawCircle = function(e){
-	ctx.beginPath();
-	console.log(e.clientX);
-	ctx.arc(e.clientX, e.clientY, 5, 0, Math.PI * 2);
-	ctx.stroke();
-	ctx.fillStyle = 'green';
-	ctx.fill();
-	return;
+    console.log(" draw_dot is now...");
+    console.log(draw_dot);
 }
 
-    var drawRect = function(e){
-	ctx.fillStyle = "black";
-	ctx.fillRect(e.clientX, e.clientY, 10, 10);
-    }
-	
+
+var artichoke = function(e){
+    var x2 = e.offsetX;
+    var y2 = e.offsetY;
+    moveTo(x2, y2);
+    
 
 
-//for (i=0; i<10; i++){
-//  if (i%2 === 0){
-//ctx.fillStyle = "#FFF089";
-//ctx.fillRect(i, i, 200, 200); }
-//  else {
-//ctx.fillStyle = "#FBB009";
-//ctx.fillRect(i, i, 200, 200);
-//  }
-//}
+}
 
+var drawCircle = function(e){
 
-//ctx.beginPath();
-//ctx.arc(100, 100, 35, 0, Math.PI * 2);
-//ctx.stroke();
-//ctx.fillStyle = 'green';
-//ctx.fill();
-//ctx.beginPath();
-//ctx.arc(100, 110, 15, 0, Math.PI);
-//ctx.fillStyle = 'white';
-//ctx.fill();
-//ctx.beginPath();
-//ctx.arc(87, 100, 6, 0, Math.PI * 2);
-//ctx.stroke();
-//ctx.fillStyle = 'black';
-//ctx.fill();
-//ctx.beginPath();
-//ctx.arc(115, 100, 6, 0, Math.PI * 2);
-//ctx.stroke();
-//ctx.fillStyle = 'black';
-//ctx.fill();
+    ctx.arc(e.clientX, e.clientY, 5, 0, Math.PI * 2);
+    
+    ctx.stroke();
+    ctx.fillStyle = 'green';
+    ctx.fill();
+    ctx.closePath();
+
+    return;
+}
+
+var drawRect = function(e){
+    
+    ctx.fillStyle = "black";
+    ctx.fillRect(e.clientX, e.clientY, 10, 10);
+    ctx.closePath();
+
+}
+
